b13235d601f12ca49962395d835728aaf0fd274a
Billy <billy@Bills-Mac-mini.local>
2018-11-14 19:45:29 -0600

    Register User

--- Project/Messender/app/src/main/java/csc472/depaul/edu/messender/MainActivity.java
+++ Project/Messender/app/src/main/java/csc472/depaul/edu/messender/MainActivity.java
@@ -1,13 +1,30 @@
 package csc472.depaul.edu.messender;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
 
+    //private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.start_activity);
+
+        //mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    protected void onStart() {
+
+        super.onStart();
+
+        //check if user is signed in and update UI
+        //FirebaseUser currentUser = mAuth.getCurrentUser();
+        //TODO
     }
 }
