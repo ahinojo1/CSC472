db9f685531bd41c79ba6f065b0e4457caf064ce4
Billy <billy@Bills-Mac-mini.local>
2018-11-15 22:58:56 -0600

    added start activity that directs to login and register activities

--- Project/Messender/app/src/main/java/csc472/depaul/edu/messender/MainActivity.java
+++ Project/Messender/app/src/main/java/csc472/depaul/edu/messender/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.start_activity);
+        setContentView(R.layout.activity_start);
 
         //mAuth = FirebaseAuth.getInstance();
     }
